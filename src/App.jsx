4b2bb1ddfe0405ced1dd364ddda786f756438630
sairaj730 <vadasairaju123@gmail.com>
2025-04-03 10:53:10 +0530

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
 export const App = () => {
-  return <h1>Hello, Sai Raj!</h1>;
+  return (
+    <div >
+      <div><img src="/img/ghibli art.jpg" alt="ghibli" height="10%" width="10%"  />
+      </div>
+    <h2>Name: Stranger things</h2>
+    <h3>Rating: 9.2</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+       Quaerat repellat dolores, sit, culpa odit nulla reprehenderit numquam temporibus,
+       recusandae aliquid voluptatem aperiam illo aut ratione vitae perspiciatis. Nemo, doloribus quam.</p>
+    </div>
+  );
 };
