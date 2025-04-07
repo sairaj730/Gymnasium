e7e407f9823437da9a308e11bde3b65f0a0ab2a2
sairaj730 <vadasairaju123@gmail.com>
2025-04-07 10:14:02 +0530

    updated changes

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,11 +1,11 @@
 export const SeriesCard = ({curElem}) => {
     const {img_url, name, rating, description, cast, genre, watch_url} = curElem;
     return (
-        <div>
+        <div className="card-content">
             <li className="card">
                 <img src={img_url} alt={name} width="30%" height="30%" />
                 <h3><strong>Series name: </strong> {name}</h3>
-                <p><strong>⭐ Rating:</strong> {rating}</p>
+                <p><strong>⭐ Rating:</strong> <span className="super_hit">{rating}</span></p>
                 <p><strong>Summary: </strong> {description}</p>
                 <p><strong>Cast: </strong> {cast }</p>
                 <p><strong>Genre:</strong> {genre}</p>
